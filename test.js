51cc3863d162c490193c99288492c22d9cc36611
Simran Verma <simranverma.sv17@gmail.com>
2025-07-07 15:33:11 +0530

    Add new file

---
+++ test.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "sim@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "ran@gmail.com"
+accountPassword = "121212"
+accountCity = "Goa"
+
+console.log(accountId);
+
+console.log([accountId, accountEmail, accountPassword, accountCity])
